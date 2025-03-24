38b4f3d7c9208f14131712792ce02f66b0cc97be
AlexanderToles <alexandertoleseducation@gmail.com>
2025-03-24 17:52:44 -0500

    updated comparescores

--- problem1.c
+++ problem1.c
@@ -1,45 +1,91 @@
 #include <stdio.h>
 void printCombinations(int td,int fg,int s,int td2,int td1){
-    printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3 pt FG %d Safety\n", td2, td1, td, fg, s);
+    printf("%d TD + 2pt, %d TD + FG, %d TD, %d FG %d Safety\n", td2, td1, td, fg, s);
 }
-
+void compareScores(int i, int score, int s,int fg, int td, int td1, int td2){
+    int testScore[] = {2,3,6,7,8};
+    if(score >= score-testScore[i]){
+        if(score-testScore[i]<0){
+            compareScores(0,score,s,fg,td,td1,td2);
+        }
+        else{
+            switch(score-testScore[i]){
+                case 0:
+                switch(testScore[i]){
+                    case 2:
+                        s++;
+                        //printf("s: %d\n",s);
+                        break;
+                    case 3:
+                        fg++;
+                        //printf("fg: %d\n",fg);
+                        break;
+                    case 6:
+                        td++;
+                        //printf("td: %d\n",td);
+                        break;
+                    case 7:
+                        td1++;
+                        //printf("td1: %d\n",td1);
+                        break;
+                    case 8:
+                        td2++;
+                        //printf("td2: %d\n",td2);
+                        break;
+                }
+                    //printf("Score: %d, testScore: %d\n, score-testScore[i]: %d\n", score,testScore[i], score-testScore[i]);
+                    printCombinations(td,fg,s,td2,td1);
+                    break;
+                case 1:
+                    //printf("Score: %d, testScore: %d\n, score-testScore[i]: %d\n", score,testScore[i], score-testScore[i]);
+                    i++;
+                    compareScores(i,score,s,fg,td,td1,td2);
+                    break;
+                default:
+                switch(testScore[i]){
+                    case 2:
+                        s++;
+                        //printf("s: %d\n",s);
+                        break;
+                    case 3:
+                        fg++;
+                        //printf("fg: %d\n",fg);
+                        break;
+                    case 6:
+                        td++;
+                        //printf("td: %d\n",td);
+                        break;
+                    case 7:
+                        td1++;
+                        //printf("td1: %d\n",td1);
+                        break;
+                    case 8:
+                        td2++;
+                        //printf("td2: %d\n",td2);
+                        break;
+                }
+                    //printf("Score: %d, testScore: %d\n, score-testScore[i]: %d\n", score,testScore[i], score-testScore[i]);
+                    score = score-testScore[i];
+                    compareScores(i,score,s,fg,td,td1,td2);
+                    break;
+            }
+        }
+        }
+        
+    
+}
 void searchCombinations(int score){
     int td,fg,s,td2,td1;
     td = fg = s =td2 = td1 = 0;
     int testScore[] = {2,3,6,7,8};
-    int i = 0;
-    while(testScore[i] <= score){
-        switch(i){
-            case 2:
-                s++;
-                break;
-            case 3:
-                fg++;
-                break;
-            case 6:
-                td++;
-                break;
-            case 7:
-                td1++;
-                break;
-            case 8:
-                td2++;
-                break;
-        }
-        printf("\n%d", i);
-        switch(score-testScore[i]){
-            case 0:
-                printf("Score: %d, testScore: %d\n, score-testScore[i]: %d", score,testScore[i], score-testScore[i]);
-                printCombinations(td,fg,s,td2,td1);
-                break;
-            case 1:
-                i++;
-                break;
-            default:
-                score = score-testScore[i];
-                break;
+    
+    for(int i = 0;i<5;i++){
+        if(score >= testScore[i]){
+            compareScores(i,score,s,fg,td,td1,td2);
         }
     }
+    
+    
     
 
 }
