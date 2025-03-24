768529a89602f9df3b87d6489bd51af98bef3ab3
AlexanderToles <alexandertoleseducation@gmail.com>
2025-03-24 17:03:57 -0500

    working on searchCombinations

--- problem1.c
+++ problem1.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
 void printCombinations(int td,int fg,int s,int td2,int td1){
-    printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3 pt FG %d Safety\n", &td2, &td1, &td, &fg, &s);
+    printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3 pt FG %d Safety\n", td2, td1, td, fg, s);
 }
+
 void searchCombinations(int score){
     int td,fg,s,td2,td1;
-    if(score%3==0){
-        fg = score/3;
+    td = fg = s =td2 = td1 = 0;
+    int testScore[] = {2,3,6,7,8};
+    int i = 0;
+    while(testScore[i] <= score){
+        switch(i){
+            case 2:
+                s++;
+                break;
+            case 3:
+                fg++;
+                break;
+            case 6:
+                td++;
+                break;
+            case 7:
+                td1++;
+                break;
+            case 8:
+                td2++;
+                break;
+        }
+        printf("\n%d", i);
+        switch(score-testScore[i]){
+            case 0:
+                printf("Score: %d, testScore: %d\n, score-testScore[i]: %d", score,testScore[i], score-testScore[i]);
+                printCombinations(td,fg,s,td2,td1);
+                break;
+            case 1:
+                i++;
+                break;
+            default:
+                score = score-testScore[i];
+                break;
+        }
     }
-    if(score%2==0){
-        s=score/2;
-    }
-    if(score%6==0){
-        td=score/6;
-    }
-    if(score%8==0){
-        td2=score/8;
-    }
-    if(score%7==0){
-        td1=score/7;
-    }
+    
+
 }
 int main(){
     int score;
     printf("Enter Score: ");
     scanf("%d", &score);
-    printf("\n%d", score);
+    searchCombinations(score);
     return 0;
 }
