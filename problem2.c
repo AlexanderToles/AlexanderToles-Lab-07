8d47c51a2caadb5f24cec1fb148aeb22d0ef88db
AlexanderToles <alexandertoleseducation@gmail.com>
2025-03-25 11:54:28 -0500

    created basic error handling for menu selection

--- problem2.c
+++ problem2.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+
+
+
+int validateSelect(int tempScale, int convTarget){
+    int retryVal = 0;
+    if(tempScale > 3 || tempScale < 1){
+        printf("Invalid selection for temperature scale. (Value must be between 1-3)\n");
+        retryVal = 1;
+    }
+    if(convTarget > 3 || convTarget < 1){
+        printf("Invalid selection for conversion target. (Value must be between 1-3)\n");
+        retryVal = 1;
+    }
+    return retryVal;
+}
+
 void printMenu(){
     float tempVal;
     int tempScale;
@@ -9,7 +25,13 @@ void printMenu(){
     scanf("%d",&tempScale);
     printf("\nConvert to: \n\t1)Celcius\n\t2)Fahrenheit\n\t3)Kelvin\nSelection(1-3): ");
     scanf("%d",&convTarget);
+
+    if(validateSelect(tempScale,convTarget) == 1){
+        printMenu();
+    }
 }
+
+
 
 int main(){
     printMenu();
