d28bc017fa3398c483beeb5c72f6182e86105f48
AlexanderToles <alexandertoleseducation@gmail.com>
2025-03-25 11:43:55 -0500

    Created inputs and menu

--- problem2.c
+++ problem2.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+void printMenu(){
+    float tempVal;
+    int tempScale;
+    int convTarget;
+    printf("Enter temperature: ");
+    scanf("%f",&tempVal);
+    printf("\nChoose current scale: \n\t1)Celcius\n\t2)Fahrenheit\n\t3)Kelvin\nSelection(1-3): ");
+    scanf("%d",&tempScale);
+    printf("\nConvert to: \n\t1)Celcius\n\t2)Fahrenheit\n\t3)Kelvin\nSelection(1-3): ");
+    scanf("%d",&convTarget);
+}
+
 int main(){
+    printMenu();
     return 0;
 }
